b94753aeaf6aa3ad8384a32da044d3feb4f51d92
elena <573200159@qq.com>
2024-10-18 10:35:43 +0800

    创建公用ShaderProgram类

--- openglLibrary/src/main/java/com/opengllib/programs/ShaderProgram.kt
+++ openglLibrary/src/main/java/com/opengllib/programs/ShaderProgram.kt
@@ -1,6 +1,7 @@
 package com.opengllib.programs
 
 import android.content.Context
+import android.opengl.GLES20
 import android.opengl.GLES20.glUseProgram
 import android.util.Log
 import com.opengllib.util.ShaderHelper
@@ -10,10 +11,11 @@ open class ShaderProgram(
     context: Context, vertexShaderResourceId: Int?,
     fragmentShaderResourceId: Int?
 ) {
-    companion object{
+    companion object {
         const val TAG = "ShaderProgram"
         const val U_MATRIX = "u_Matrix"
         const val U_TIME = "u_Time"
+        const val U_COLOR = "u_Color"
         const val U_TEXTURE_UNIT = "u_TextureUnit"
         const val A_POSITION = "a_Position"
         const val A_COLOR = "a_Color"
@@ -24,8 +26,29 @@ open class ShaderProgram(
         readTextFileFromResource(context, vertexShaderResourceId!!)!!,
         readTextFileFromResource(context, fragmentShaderResourceId!!)!!
     )
+    var aPositionLocation = 0
+    var aColorLocation = 0
+    var uColorLocation = 0
+
+    init {
+        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION)
+        aColorLocation = GLES20.glGetAttribLocation(program, A_COLOR)
+        uColorLocation = GLES20.glGetUniformLocation(program, U_COLOR)
+    }
 
     fun useProgram() {
         glUseProgram(program)
+    }
+
+    fun getPositionAttributeLocation(): Int {
+        return aPositionLocation
+    }
+
+    fun getColorAttributeLocation(): Int {
+        return aColorLocation
+    }
+
+    fun getColorUniformLocation(): Int {
+        return uColorLocation
     }
 }
