bd7e624a007b8bb3621078d94e1866d775d1d63c
elena <573200159@qq.com>
2024-10-27 22:04:54 +0800

    生成图片纹理，并通过默认缓冲渲染到屏幕上进行显示。

--- fbodemo/src/main/java/com/example/fbodemo/FBOActivity.kt
+++ fbodemo/src/main/java/com/example/fbodemo/FBOActivity.kt
@@ -7,14 +7,10 @@ import com.example.fbodemo.renderer.TextureRenderer
 
 class FBOActivity : Activity() {
 
-//    private var mFBOGLSurfaceView: GLSurfaceView? = null
     private var mTextureGLSurfaceView: GLSurfaceView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout_main)
-//        mFBOGLSurfaceView = findViewById(R.id.fbo)
-//        mFBOGLSurfaceView!!.setEGLContextClientVersion(3)
-//        mFBOGLSurfaceView!!.setRenderer(FBORenderer(this))
         mTextureGLSurfaceView = findViewById(R.id.texture)
         mTextureGLSurfaceView!!.setEGLContextClientVersion(3)
         mTextureGLSurfaceView!!.setRenderer(TextureRenderer(this))
@@ -22,12 +18,10 @@ class FBOActivity : Activity() {
 
     override fun onResume() {
         super.onResume()
-//        mFBOGLSurfaceView!!.onResume()
     }
 
     override fun onPause() {
         super.onPause()
-//        mFBOGLSurfaceView!!.onPause()
     }
 
     override fun onDestroy() {
