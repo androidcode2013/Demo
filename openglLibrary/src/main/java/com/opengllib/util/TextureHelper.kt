b554635b7f80b03adf3b1d22f6a412ef02dc5df5
elena <573200159@qq.com>
2024-11-13 18:37:35 +0800

    add： 1、创建MatrixHelper矩阵工具类 2、TextureHelper类中添加加载立方体贴图方法loadCubemap

--- openglLibrary/src/main/java/com/opengllib/util/TextureHelper.kt
+++ openglLibrary/src/main/java/com/opengllib/util/TextureHelper.kt
@@ -13,10 +13,40 @@ import android.opengl.GLES20.glDeleteTextures
 import android.opengl.GLES20.glGenTextures
 import android.opengl.GLES20.glGenerateMipmap
 import android.opengl.GLES20.glTexParameteri
+import android.opengl.GLES30
+import android.opengl.GLUtils
 import android.opengl.GLUtils.texImage2D
 import android.util.Log
 
 private val TAG = "TextureHelper"
+
+
+fun loadCubemap(context: Context, resourceIds: IntArray): Int {
+    val textureObjectIds = IntArray(1)
+    glGenTextures(1, textureObjectIds, 0)
+    if (textureObjectIds[0] == 0) {
+        glDeleteTextures(1, textureObjectIds, 0)
+        return 0
+    }
+    GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
+    glBindTexture(GLES30.GL_TEXTURE_CUBE_MAP, textureObjectIds[0])
+
+    for (i in resourceIds.indices) {
+        var bitmap = generateBitmap(context, resourceIds[i])
+        GLUtils.texImage2D(GLES30.GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, bitmap, 0)
+        bitmap.recycle()
+    }
+    glTexParameteri(GLES30.GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GLES30.GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GLES30.GL_TEXTURE_CUBE_MAP, GLES30.GL_TEXTURE_WRAP_S, GLES30.GL_CLAMP_TO_EDGE);
+    glTexParameteri(GLES30.GL_TEXTURE_CUBE_MAP, GLES30.GL_TEXTURE_WRAP_T, GLES30.GL_CLAMP_TO_EDGE);
+    glTexParameteri(GLES30.GL_TEXTURE_CUBE_MAP, GLES30.GL_TEXTURE_WRAP_R, GLES30.GL_CLAMP_TO_EDGE);
+
+    glBindTexture(GLES30.GL_TEXTURE_CUBE_MAP, 0);
+
+    return textureObjectIds[0];
+}
+
 fun loadTexture(context: Context, resourceId: Int): Int {
     var bitmap = generateBitmap(context, resourceId)
     if (bitmap == null) {
