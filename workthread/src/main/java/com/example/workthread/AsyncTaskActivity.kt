2594287d3f3fb39c0a71ec8a7029b092ebdb8fc8
elena <573200159@qq.com>
2024-12-17 18:32:26 +0800

    AsyncTask通过创建自定义线程池处理任务 参考文章： https://blog.csdn.net/github_27263697/article/details/144420406

--- workthread/src/main/java/com/example/workthread/AsyncTaskActivity.kt
+++ workthread/src/main/java/com/example/workthread/AsyncTaskActivity.kt
@@ -5,6 +5,11 @@ import android.os.AsyncTask
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import java.util.concurrent.Executor
+import java.util.concurrent.LinkedBlockingDeque
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+
 
 class AsyncTaskActivity : Activity() {
 
@@ -12,8 +17,14 @@ class AsyncTaskActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
         var myAsyncTask = MyAsyncTask()
-        myAsyncTask.execute()
-
+        //默认使用线程池THREAD_POOL_EXECUTOR
+//        myAsyncTask.execute()
+        //创建自定义线程池
+        val executor: Executor = ThreadPoolExecutor(
+            10, 50, 10,
+            TimeUnit.SECONDS, LinkedBlockingDeque<Runnable>(100)
+        )
+        myAsyncTask.executeOnExecutor(executor)
     }
 
     inner class MyAsyncTask : AsyncTask<String, Int, Long>() {
@@ -21,14 +32,14 @@ class AsyncTaskActivity : Activity() {
         override fun onPostExecute(result: Long?) {
             super.onPostExecute(result)
             //运行在主线程
-            Log.d("tag", "whx#result:" + result)
-            Log.d("tag", "whx#onPostExecute#thread: ${Thread.currentThread()}")
+            Log.d("tag", "result:" + result)
+            Log.d("tag", "onPostExecute#thread: ${Thread.currentThread()}")
             this@AsyncTaskActivity.findViewById<TextView>(R.id.showtext).text = result.toString()
         }
 
         override fun doInBackground(vararg params: String?): Long {
             //运行在子线程
-            Log.d("tag", "whx#doInBackground#thread: ${Thread.currentThread()}")
+            Log.d("tag", "doInBackground#thread: ${Thread.currentThread()}")
             return 1000
         }
     }
